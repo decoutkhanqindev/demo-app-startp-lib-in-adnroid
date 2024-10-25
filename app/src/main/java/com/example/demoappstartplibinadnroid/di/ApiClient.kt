df13404c38c60f5ee06b3e68b78b79d9cb9974d5
MeansKane <phamminhkhangpy2003@gmail.com>
2024-10-25 14:46:58 +0700

    Add ApiClient singleton with Retrofit and OkHttpClient

---
+++ app/src/main/java/com/example/demoappstartplibinadnroid/di/ApiClient.kt
@@ -0,0 +1,33 @@
+package com.example.demoappstartplibinadnroid.di
+
+import android.content.Context
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import java.io.File
+
+object ApiClient {
+  lateinit var retrofit: Retrofit
+  
+  fun initialize(applicationContext: Context, baseUrl: String) {
+    // File represents the application cache directory
+    val httpCacheDirectory = File(applicationContext.cacheDir, "http-cache")
+    val cacheSize: Long = 10 * 1024 * 1024 // 10MB
+    // Caches HTTP and HTTPS responses to the filesystem so they may be reused,
+    // saving time and bandwidth.
+    val cache = Cache(httpCacheDirectory, cacheSize)
+    
+    val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+      .cache(cache)
+      .build()
+    
+    retrofit = Retrofit.Builder()
+      .client(okHttpClient)
+      .addConverterFactory(GsonConverterFactory.create())
+      .build()
+  }
+  
+  inline fun <reified T : Any> create(): T = retrofit.create<T>()
+}
