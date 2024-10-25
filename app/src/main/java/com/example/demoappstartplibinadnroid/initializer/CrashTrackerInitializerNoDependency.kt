05336f02724fd91e74a2a8a899675bab2e8fbb40
MeansKane <phamminhkhangpy2003@gmail.com>
2024-10-25 15:02:27 +0700

    Add App Startup Initializer for CrashTrackerService automatic initialization on app launch

--- app/src/main/java/com/example/demoappstartplibinadnroid/initializer/CrashTrackerInitializerNoDependency.kt
+++ app/src/main/java/com/example/demoappstartplibinadnroid/initializer/CrashTrackerInitializerNoDependency.kt
@@ -3,9 +3,11 @@ package com.example.demoappstartplibinadnroid.initializer
 import android.content.Context
 import androidx.startup.Initializer
 import com.example.demoappstartplibinadnroid.service.CrashTrackerService
+import timber.log.Timber
 
 class CrashTrackerInitializerNoDependency : Initializer<CrashTrackerService> {
   override fun create(context: Context): CrashTrackerService {
+    Timber.tag("Initializer").d("CrashTrackerService is created")
     return CrashTrackerService.initialize(context)
   }
   
