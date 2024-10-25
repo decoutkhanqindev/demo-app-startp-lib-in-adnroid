9a0b9603c731c10d1f40d6b8991fc1acf0c816ea
MeansKane <phamminhkhangpy2003@gmail.com>
2024-10-25 14:52:24 +0700

    Add App Startup Initializer for ApiClient automatic initialization on app launch

--- app/src/main/java/com/example/demoappstartplibinadnroid/initializer/ApiClientInitializer.kt
+++ app/src/main/java/com/example/demoappstartplibinadnroid/initializer/ApiClientInitializer.kt
@@ -3,10 +3,12 @@ package com.example.demoappstartplibinadnroid.initializer
 import android.content.Context
 import androidx.startup.Initializer
 import com.example.demoappstartplibinadnroid.di.ApiClient
+import timber.log.Timber
 
 class ApiClientInitializer: Initializer<ApiClient> {
   override fun create(context: Context): ApiClient {
     ApiClient.initialize(context, "https://example.com/api")
+    Timber.tag("Initializer").d("ApiClient is created.")
     return ApiClient
   }
   
