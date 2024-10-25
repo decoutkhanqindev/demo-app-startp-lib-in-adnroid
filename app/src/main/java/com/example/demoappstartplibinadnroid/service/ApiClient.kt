166697f62da085264843b8542e3c686b4e00be27
MeansKane <phamminhkhangpy2003@gmail.com>
2024-10-25 15:13:56 +0700

    Add ApiClient singleton with Retrofit and OkHttpClient

--- app/src/main/java/com/example/demoappstartplibinadnroid/service/ApiClient.kt
+++ app/src/main/java/com/example/demoappstartplibinadnroid/service/ApiClient.kt
@@ -24,6 +24,7 @@ object ApiClient {
       .build()
     
     retrofit = Retrofit.Builder()
+      .baseUrl(baseUrl)
       .client(okHttpClient)
       .addConverterFactory(GsonConverterFactory.create())
       .build()
